0b0b027c47e8f27322c9984adfbd084246027000
Ricardo Behrens <behrensricardo@gmail.com>
2023-09-23 08:12:57 -0600

    moved everything away from Main folder

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const markdown = require('')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'luckydog',
+    database: 'acme_db'
+  },
+  console.log(`Connected to the acme_db database.`)
+);
+
+// function init() {
+//     inquirer
+//         .prompt([
+//             {
+//                 type: 'input',
+//                 name: 'name',
+//                 message: 'What is your name?',
+//             },
+//         ])
+
+        
+//         .then((data) => {
+            
+
+//         });
+// }
+
+// // Function call to initialize app
+// init();
+
+// Query database
+db.query('SELECT * FROM employee', function (err, results) {
+    console.log(results);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+res.status(404).end();
+});
+
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
