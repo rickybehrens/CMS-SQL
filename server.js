4b2d617a8512378385ee2084df2877613d08f2ee
Ricardo Behrens <behrensricardo@gmail.com>
2023-09-23 16:02:10 -0600

    adding more functions to the prompt

--- server.js
+++ server.js
@@ -26,34 +26,41 @@ function init() {
         name: 'main', // Use 'main' here to match the variable name
         message: 'What would you like to do?',
         choices: [
+          'View all departments',
+          'View all roles',
           'View all employees',
-          'View all employees by department',
-          'View all employees by manager',
+          // 'View all employees by department',
+          // 'View all employees by manager',
+          'Add department',
+          'Remove department',
+          'Add role',
+          'Remove role',
           'Add employee',
           'Remove employee',
           'Update employee role',
-          'Update employee manager',
-          'View all roles',
-          'Add role',
-          'Remove role',
-          'View all departments',
-          'Add department',
-          'Remove department',
-          'View total utilized budget by department',
+          // 'Update employee manager',
+          // 'View total utilized budget by department',
           'Quit',
         ],
       },
     ])
     .then((data) => {
       switch (data.main) {
+        case 'View all departments':
+          constviewAllDepartments();
+          break;
+
+        case 'View all roles':
+          viewAllRoles()
+          break;
+
         case 'View all employees':
           viewAllEmployees();
           break;
 
-        case 'View all employees by department':
-
-          init();
-          break;
+          // case 'View all employees by department':  
+          //   viewEmployeesByDepartment()
+          //   break;
 
           init();
         case 'View all employees by manager':
@@ -80,13 +87,6 @@ function init() {
           init();
           break;
 
-        case 'View all roles':
-          db.query('SELECT * FROM role', function (err, results) {
-            console.log(results);
-          });
-          init();
-          break;
-
         case 'Add role':
 
           init();
@@ -94,13 +94,6 @@ function init() {
 
         case 'Remove role':
 
-          init();
-          break;
-
-        case 'View all departments':
-          db.query('SELECT * FROM department', function (err, results) {
-            console.log(results);
-          });
           init();
           break;
 
@@ -129,14 +122,50 @@ function init() {
 // Function call to initialize app
 init();
 
+// view all departments
+// view all roles
+// view all employees
+// add a department
+// remove department
+// add a role
+// remove role
+// add an employee
+// remove employee
+// update an employee role
+// quit
+
+// View All Departments
+constviewAllDepartments = () => {
+  db.query('SELECT * FROM department', function (err, response) {
+    console.table(response);
+  });
+  init();
+};
+
+// View All Roles
+const viewAllRoles = () => {
+  db.query('SELECT * FROM role', function (err, results) {
+    console.table(results);
+  });
+  init();
+};
+
 // View All Employees
 const viewAllEmployees = () => {
-  const example = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS 'department', role.salary FROM employee, role, department WHERE department.id = role.department_id AND role.id = employee.role_id ORDER BY employee.id ASC`;
-  db.query(example, (error, response) => {
+  const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS 'department', CONCAT(m.first_name, ' ', m.last_name) AS 'manager', role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee m ON employee.manager_id = m.id ORDER BY employee.id ASC`;
+  db.query(sql, (error, response) => {
     console.table(response)
     init();
   });
 };
+
+// const viewEmployeesByDepartment = () => {
+//   const sql = `SELECT employee.first_name, employee.last_name, department.name AS 'department', CONCAT(m.first_name, ' ', m.last_name) AS 'manager', role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee m ON employee.manager_id = m.id ORDER BY department.name ASC`;
+//   db.query(sql, (error, response) => {
+//     console.table(response)
+//     init();
+//   });
+// };
 
 const addNewEmployee = () => {
   inquirer
