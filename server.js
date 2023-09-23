07ac0df1f34fcf001f1a6407379f067e610d23da
Ricardo Behrens <behrensricardo@gmail.com>
2023-09-23 14:50:30 -0600

    Working my way through the inquirer prompt

--- server.js
+++ server.js
@@ -47,69 +47,80 @@ function init() {
     .then((data) => {
       switch (data.main) {
         case 'View all employees':
-          db.query('SELECT * FROM employee', function (err, results) {
-            console.log(results);
-          });
+          viewAllEmployees();
           break;
 
         case 'View all employees by department':
 
+          init();
           break;
 
+          init();
         case 'View all employees by manager':
 
           break;
 
+          init();
         case 'Add employee':
           addNewEmployee();
           break;
 
         case 'Remove employee':
 
+          init();
           break;
 
         case 'Update employee role':
 
+          init();
           break;
 
         case 'Update employee manager':
 
+          init();
           break;
 
         case 'View all roles':
           db.query('SELECT * FROM role', function (err, results) {
             console.log(results);
           });
+          init();
           break;
 
         case 'Add role':
 
+          init();
           break;
 
         case 'Remove role':
 
+          init();
           break;
 
         case 'View all departments':
           db.query('SELECT * FROM department', function (err, results) {
             console.log(results);
           });
+          init();
           break;
 
         case 'Add department':
 
+          init();
           break;
 
         case 'Remove department':
 
+          init();
           break;
 
         case 'View total utilized budget by department':
 
+          init();
           break;
 
         case 'Quit':
-          console.log('quit');
+          process.exit();
           break;
       };
     });
@@ -118,95 +129,118 @@ function init() {
 // Function call to initialize app
 init();
 
+// View All Employees
+const viewAllEmployees = () => {
+  const example = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS 'department', role.salary FROM employee, role, department WHERE department.id = role.department_id AND role.id = employee.role_id ORDER BY employee.id ASC`;
+  db.query(example, (error, response) => {
+    console.table(response)
+    init();
+  });
+};
+
 const addNewEmployee = () => {
   inquirer
     .prompt([
       {
         type: "input",
-        name: "first-name",
+        name: "firstName",
         message: "What's the new employee's first name?",
       },
       {
         type: "input",
-        name: "last-name",
+        name: "lastName",
         message: "What's the new employee's last name?",
       },
       {
         type: "list",
-        name: "role-id",
-        message: "What's the new employee's role?",
+        name: "department",
+        message: "What department will the new employee work?",
         choices: [
-          "Sales Person",
-          "Sales Manager",
-          "Market Analyst",
-          "Senior Market Analyst",
-          "Marketing Manager",
-          "Programmer",
-          "Senior Programmer",
-          "IT Manager",
-          "Paralegal",
-          "Counsel",
-          "Chief Legal Counsel",
-          "Financial Analyst",
-          "Accounting Manager",
-          "CFO",
-          "Procurement Specialist",
-          "Supply Chain Manager",
-          "HR Coordinator",
-          "HR Manager",
-          "Line Employee",
-          "Line Manager",
-          "Production Manager",
-          "COO",
-          "CEO",
+          "Sales and Marketing",
+          "IT",
+          "Legal",
+          "Accounting",
+          "Purchasing",
+          "HR",
+          "Production",
         ],
       },
       {
         type: "list",
-        name: "manager-id",
+        name: "roleId",
+        message: "What's the new employee's role?",
+        choices: [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+          11,
+          12,
+          13,
+          14,
+          15,
+          16,
+          17,
+          18,
+          19,
+          20,
+          21,
+          22,
+          23,
+        ],
+      },
+      {
+        type: "list",
+        name: "managerId",
         message: "Who's the new employee's manager?",
         choices: [
-          "Courtney Cox",
-          "Michael Garrigus",
-          "Marcus Gambia",
-          "Al Smith",
-          "Charles Ivery",
-          "Edward Zea",
-          "Edward Walsh",
-          "Frederic Plummer",
-          "Gustav Moser",
+          2,
+          5,
+          8,
+          11,
+          14,
+          16,
+          18,
+          22,
+          23,
         ],
       },
     ])
-    .then((answers) => {
-      // Construct the SQL INSERT statement
-      const sql = `
-      INSERT INTO employee (first_name, last_name, role_id, manager_id)
-      VALUES (?, ?, ?, ?)
+    .then((employeeData) => {
+      console.log(employeeData.firstName)
+      // Create a SQL query to insert the new employee
+      const query = `
+        INSERT INTO employee (first_name, last_name, role_id, manager_id)
+        VALUES (?, ?, ?, ?)
       `;
 
-      const values = [
-        answers['first-name'],
-        answers['last-name'],
-        answers['role-id'],
-        answers['manager-id'],
-      ];
-
-      // Execute the INSERT query
-      db.query(sql, values, (err, result) => {
-        if (err) {
-          console.error('Error adding employee:', err);
-        } else {
-          console.log(`Added new employee: ${answers['first-name']} ${answers['last-name']}`);
+      // Execute the query with the provided employee data
+      db.query(
+        query,
+        [
+          employeeData.firstName,
+          employeeData.lastName,
+          employeeData.roleId,
+          employeeData.managerId || null, // Use null if manager_id is not provided
+        ],
+        (err, results) => {
+          if (err) {
+            console.error('Error adding employee:', err);
+          } else {
+            console.log('Employee added successfully!');
+          }
+          // Close the database connection
         }
-        // You can add further code here if needed
-      });
+      );
+      init();
     });
 };
-
-db.query('SELECT * FROM employee', function (err, results) {
-  console.log(results);
-});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
